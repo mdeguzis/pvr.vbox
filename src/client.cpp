aa6a315b1109eb94cd39053a1e0beacdb316dea3
Alwin Esch <alwin.esch@web.de>
2016-12-05 16:56:30 +0100

    add initial functions for instance handling (currently unused)

--- src/client.cpp
+++ src/client.cpp
@@ -304,6 +304,15 @@ extern "C" {
   {
   }
 
+  ADDON_STATUS ADDON_CreateInstance(int instanceType, const char* instanceID, const void* instanceProps, void* instanceFunctions, void* kodiInstance, void** addonInstance)
+  {
+    return ADDON_STATUS_UNKNOWN;
+  }
+
+  void ADDON_DestroyInstance(int instanceType, const char* instanceID, void* instance)
+  {
+  }
+
   void OnSystemSleep()
   {
   }
